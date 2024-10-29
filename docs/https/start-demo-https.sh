d8e13eb70d121d9b09a61e6d462de7b337b43fc5
kt-chan <ktchan@gmail.com>
2024-10-29 14:47:20 +0800

    Update start-demo-https.sh

--- docs/https/start-demo-https.sh
+++ docs/https/start-demo-https.sh
@@ -14,7 +14,6 @@ docker run -d -p 443:3001 \
 -v $HOME/anyzearch/run/.env:/app/server/.env \
 -v $HOME/anyzearch/run/.env-fe:/app/frontend/.env \
 -v $HOME/anyzearch/run/docker-entrypoint.sh:/usr/local/bin/docker-entrypoint.sh \
--v $HOME/anyzearch/run/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt \
 -v $HOME/anyzearch/run/anyzearch.local.crt:/etc/ssl/certs/anyzearch.local.crt \
 -v $HOME/anyzearch/run/anyzearch.local.key:/etc/ssl/certs/anyzearch.local.key \
 -e STORAGE_DIR="/app/server/storage" \
