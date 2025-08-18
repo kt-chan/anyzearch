6b11f4d04494d7383dd493f509b38a75c782ccde
kt-chan <ktchan@gmail.com>
2025-08-18 11:09:15 +0800

    Update env.sh

--- run/env.sh
+++ run/env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export STORAGE_LOCATION=$HOME/anyzearch/data/
-mkdir -p $STORAGE_LOCATION
+mkdir -p $STORAGE_LOCATION/app
+mkdir -p $STORAGE_LOCATION/db
 sudo chown 1000:1000 -R $STORAGE_LOCATION  
 sudo chown 1000:1000 ./.env ./.env-fe
 
